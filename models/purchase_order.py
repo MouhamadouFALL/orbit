15ea55a3905fb21280b496262010fe6effa28b3b
Mouhamadou FALL <mouhamadou2.fall@gmail.com>
2025-04-03 20:10:09 +0000

    Modifier lecture seule avant confirmation bon d'achat

--- models/purchase_order.py
+++ models/purchase_order.py
@@ -9,24 +9,29 @@ class PurchaseOrder(models.Model):
     attachment_ids = fields.Many2many('ir.attachment', 'orbit_attachment_rel', 'orbit_id', 'attachment_id', string="Pieces jointes", store=True, help="Attach files related to this order")
 
     usr_confirmed = fields.Many2one('res.users', string="Confirmé par", readonly=True)
+    is_locked = fields.Boolean(string="Verrouillé", compute="_compute_is_locked", store=True, help="Indique si le bon d'achat est verrouillé en lecture seule.")
+    @api.depends('state')
+    def _compute_is_locked(self):
+        for order in self:
+            if order.state in ['purchase', 'done']:
+                order.is_locked = True
+            else:
+                order.is_locked = False
+
+    
 
     def write(self, vals):
-        if self.state in ['purchase', 'done'] and not self.env.context.get('force_edit'):
-            raise UserError(_("Ce bon d'achat est définitivement verrouillé en lecture seule."))
-        return super().write(vals)
-
-    # def write(self, vals):
-    #     # Autoriser spécifiquement l'annulation
-    #     if vals.get('state') == 'cancel':
-    #         return super().write(vals)
+        # Autoriser spécifiquement l'annulation
+        if vals.get('state') == 'cancel':
+            return super().write(vals)
         
-    #     if not self.env.context.get('bypass_purchase_lock'):
-    #         for order in self.filtered(lambda o: o.state in ['purchase', 'done']):
-    #             # if not self.env.user.has_group('purchase.group_purchase_manager'):
-    #             protected_fields = set(vals.keys()) - self._get_whitelisted_fields()
-    #             if protected_fields:
-    #                 raise ValidationError(_("Opération bloquée ! La commande %s est confirmée (État: %s).") % (order.name, order.state))
-    #     return super().write(vals)
+        if not self.env.context.get('bypass_purchase_lock'):
+            for order in self.filtered(lambda o: o.state in ['purchase', 'done']):
+                # if not self.env.user.has_group('purchase.group_purchase_manager'):
+                protected_fields = set(vals.keys()) - self._get_whitelisted_fields()
+                if protected_fields and order.is_locked:
+                    raise ValidationError(_("Opération bloquée ! La commande %s est confirmée (État: %s).") % (order.name, order.state))
+        return super().write(vals)
 
     # def _get_whitelisted_fields(self):
     #     """Champs modifiables après confirmation"""
@@ -42,7 +47,6 @@ class PurchaseOrder(models.Model):
         """Overrides the confirm button method to record the user who confirmed."""
         res = super().button_confirm()
         self.write({
-            'state': 'purchase',
             'usr_confirmed': self.env.user.id,
             })
         
