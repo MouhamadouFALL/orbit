b000383882138caa9b0773f503552c6e3607b25d
Mouhamadou FALL <mouhamadou2.fall@gmail.com>
2025-04-03 16:11:29 +0000

    lecture seul apres confirmation

--- models/purchase_order.py
+++ models/purchase_order.py
@@ -11,27 +11,32 @@ class PurchaseOrder(models.Model):
     usr_confirmed = fields.Many2one('res.users', string="Confirmé par", readonly=True)
 
     def write(self, vals):
-        # Autoriser spécifiquement l'annulation
-        if vals.get('state') == 'cancel':
-            return super().write(vals)
-        
-        if not self.env.context.get('bypass_purchase_lock'):
-            for order in self.filtered(lambda o: o.state in ['purchase', 'done']):
-                # if not self.env.user.has_group('purchase.group_purchase_manager'):
-                protected_fields = set(vals.keys()) - self._get_whitelisted_fields()
-                if protected_fields:
-                    raise ValidationError(_("Opération bloquée ! La commande %s est confirmée (État: %s).") % (order.name, order.state))
+        if self.state in ['purchase', 'done'] and not self.env.context.get('force_edit'):
+            raise UserError(_("Ce bon d'achat est définitivement verrouillé en lecture seule."))
         return super().write(vals)
 
-    def _get_whitelisted_fields(self):
-        """Champs modifiables après confirmation"""
-        return {
-            'notes',    # Notes internes
-            'state',    # État de la commande
-            # 'date_planned',  # Dates logistiques
-            # 'incoterm_id',
-            # 'priority'      # Priorité logistique
-        }
+    # def write(self, vals):
+    #     # Autoriser spécifiquement l'annulation
+    #     if vals.get('state') == 'cancel':
+    #         return super().write(vals)
+        
+    #     if not self.env.context.get('bypass_purchase_lock'):
+    #         for order in self.filtered(lambda o: o.state in ['purchase', 'done']):
+    #             # if not self.env.user.has_group('purchase.group_purchase_manager'):
+    #             protected_fields = set(vals.keys()) - self._get_whitelisted_fields()
+    #             if protected_fields:
+    #                 raise ValidationError(_("Opération bloquée ! La commande %s est confirmée (État: %s).") % (order.name, order.state))
+    #     return super().write(vals)
+
+    # def _get_whitelisted_fields(self):
+    #     """Champs modifiables après confirmation"""
+    #     return {
+    #         'notes',    # Notes internes
+    #         'state',    # État de la commande
+    #         # 'date_planned',  # Dates logistiques
+    #         # 'incoterm_id',
+    #         # 'priority'      # Priorité logistique
+    #     }
 
     def button_confirm(self):
         """Overrides the confirm button method to record the user who confirmed."""
