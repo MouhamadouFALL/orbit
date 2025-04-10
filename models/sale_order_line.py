07d5d8d1f223af510c5173cd8ae23e6d051da757
Mouhamadou FALL <mouhamadou2.fall@gmail.com>
2025-04-10 09:59:10 +0000

    Enlever les restrictions sur la suppression de produits contrer par sale order line

--- models/sale_order_line.py
+++ models/sale_order_line.py
@@ -9,7 +9,7 @@ class SaleOrderLine(models.Model):
     product_id = fields.Many2one(
         comodel_name='product.product',
         string="Product",
-        change_default=True, ondelete='restrict', check_company=True, index='btree_not_null',
+        change_default=True, ondelete='set null', check_company=True, index='btree_not_null',
         domain="[('sale_ok', '=', True), '|', ('company_id', '=', False), ('company_id', '=', company_id)]")
     
     state = fields.Selection(
