1e44e117b84d9120de1b7bc58bf008153c4b4ca5
Mouhamadou FALL <116067549+CCBMTECH@users.noreply.github.com>
2025-04-17 15:31:24 +0000

    gestion de type de produit avec les options product et service

--- models/account_move_line.py
+++ models/account_move_line.py
@@ -5,16 +5,16 @@ from odoo import models, fields, api, _
 class AccountMoveLine(models.Model):
     _inherit = "account.move.line"
     
-    # product_id = fields.Many2one(
-    #     comodel_name='product.product',
-    #     string='Product',
-    #     inverse='_inverse_product_id',
-    #     ondelete='set null',
-    # )
-    # product_uom_id = fields.Many2one(
-    #     comodel_name='uom.uom',
-    #     string='Unit of Measure',
-    #     compute='_compute_product_uom_id', store=True, readonly=False, precompute=True,
-    #     domain="[('category_id', '=', product_uom_category_id)]",
-    #     ondelete="set null",
-    # )
+    product_id = fields.Many2one(
+        comodel_name='product.product',
+        string='Product',
+        inverse='_inverse_product_id',
+        ondelete='set null',
+    )
+    product_uom_id = fields.Many2one(
+        comodel_name='uom.uom',
+        string='Unit of Measure',
+        compute='_compute_product_uom_id', store=True, readonly=False, precompute=True,
+        domain="[('category_id', '=', product_uom_category_id)]",
+        ondelete="set null",
+    )
