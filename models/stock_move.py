7a16591e1b5ceed621110cbe8b5b00df194b5480
Mouhamadou FALL <116067549+CCBMTECH@users.noreply.github.com>
2025-04-24 11:11:42 +0000

    FIX BUG: ne pas afficher ajouter une ligne sur le bon de reception

--- models/stock_move.py
+++ models/stock_move.py
@@ -1,5 +1,7 @@
 #-*- coding: utf-8 -*-
 from odoo import models, fields, api, _
+from odoo.exceptions import UserError, ValidationError
+from odoo.tools import float_round, float_compare, formatLang, format_date, pycompat
 
 
 class StockMove(models.Model):
@@ -10,3 +12,12 @@ class StockMove(models.Model):
         check_company=True,
         domain="[('type', 'in', ['product', 'service']), '|', ('company_id', '=', False), ('company_id', '=', company_id)]", index=True, required=False,
         states={'done': [('readonly', True)]})
+    
+    
+    @api.model_create_multi
+    def create(self, vals_list):
+        for vals in vals_list:
+            if vals.get('picking_id') and vals.get('picking_id')[1] and -\
+                self.env['stock.picking'].browse(vals['picking_id'][1]).picking_type_code == 'incoming':
+                raise UserError("Ajout de lignes interdit sur les bons de réception.")
+        return super().create(vals_list)
