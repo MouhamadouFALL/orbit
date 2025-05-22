88a8c6dd33049aa8f17da4175610d24f3aacfff0
Mouhamadou FALL <116067549+CCBMTECH@users.noreply.github.com>
2025-05-22 11:43:10 +0000

    ne pas autoriser la validation du bon de livraison si la quantité est inférieure ou égale à 0

--- models/stock_picking.py
+++ models/stock_picking.py
@@ -4,11 +4,11 @@ from odoo.exceptions import UserError
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
-    def action_confirm(self):
-        # Vérifiez chaque ligne de mouvement avant la confirmation de la livraison
+    def button_validate(self):
+        # Vérifier les stocks avant validation de la livraison
         for picking in self:
             if picking.picking_type_id.code != 'outgoing':
-                continue  # Ignorer les types de mouvement non sortants
+                continue  # Ignorer les mouvements non sortants
             
             for move in picking.move_ids_without_package:
                 product = move.product_id
@@ -17,11 +17,30 @@ class StockPicking(models.Model):
 
                 if available_qty <= 0:
                     raise UserError(_(
-                        "Impossible de confirmer ou livrer le produit '%s' car il n'a pas de stock .\n"
-                        "Quantité disponible : %s.\n"
-                        "Vérifiez les stocks avant de confirmer la commande ou la livraison. \n"
-                        "Vous pouvez également contacter le gestionnaire de stock pour plus d'informations."
+                        "La quantité disponible du produit '%s' est insuffisante (%s).\n"
+                        "La livraison ne peut pas être validée."
                     ) % (product.name, available_qty))
         
-        return super(StockPicking, self).action_confirm()
+        return super(StockPicking, self).button_validate()
+    
+    # def action_confirm(self):
+    #     # Vérifiez chaque ligne de mouvement avant la confirmation de la livraison
+    #     for picking in self:
+    #         if picking.picking_type_id.code != 'outgoing':
+    #             continue  # Ignorer les types de mouvement non sortants
+            
+    #         for move in picking.move_ids_without_package:
+    #             product = move.product_id
+    #             location = picking.location_id
+    #             available_qty = product.with_context(location=location.id).qty_available
+
+    #             if available_qty <= 0:
+    #                 raise UserError(_(
+    #                     "Impossible de confirmer ou livrer le produit '%s' car il n'a pas de stock .\n"
+    #                     "Quantité disponible : %s.\n"
+    #                     "Vérifiez les stocks avant de confirmer la commande ou la livraison. \n"
+    #                     "Vous pouvez également contacter le gestionnaire de stock pour plus d'informations."
+    #                 ) % (product.name, available_qty))
+        
+    #     return super(StockPicking, self).action_confirm()
     
